2e125eef435bfec6ff8a4deac081da5b50ed259c
Arjav Patel <arjav1528@gmail.com>
2025-05-18 13:33:46 +0530

    basic tracking of all the ids

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -37,6 +37,14 @@ while True:
     # print(results.multi_hand_landmarks)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id,lm in enumerate(handLms.landmark):
+                h,w,c = img.shape
+                cx,cy = int(lm.x*w),int(lm.y*h)
+                print(id,cx,cy)
+                if id == 0:
+                    cv2.circle(img,(cx,cy),20,(255,0,255),-1)
+
+
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     cv2.imshow("image", img)

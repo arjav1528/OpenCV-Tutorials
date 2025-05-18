0ea0f246bc8dfb20f0aa2fb46899669e6c261d03
Arjav Patel <arjav1528@gmail.com>
2025-05-18 16:26:54 +0530

    Full Hand Tracking Module Complete

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -48,19 +48,44 @@ def main():
 
     detector = handDetector()
 
+    count = [0,0,0,0,0]
+
     while True:
         currTime = time.time()
         fps = int(1/(currTime-prevTime))
         prevTime = currTime
         success, img = cap.read()
         img = cv2.flip(img,1)
-        cv2.putText(img,str(fps),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
+        cv2.putText(img,f"FPS : {fps}",(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
 
 
         finalImg = detector.findHands(img)
         lmList = detector.findPosition(finalImg)
         if len(lmList)!=0:
-            print(lmList[4])
+            if(lmList[4][1]<lmList[3][1] & lmList[3][1]<lmList[2][1] & lmList[2][1]<lmList[1][1]):
+                # print(lmList[4][1],lmList[3][1],lmList[2][1],lmList[1][1])
+                count[0] = 1
+            else:
+                count[0] = 0
+            if(lmList[8][2] < lmList[7][2] & lmList[7][2] < lmList[6][2] & lmList[6][2] < lmList[5][2]):
+                count[1] = 1
+            else:
+                count[1] = 0
+            if(lmList[12][2] < lmList[11][2] & lmList[11][2] < lmList[10][2] & lmList[10][2] < lmList[9][2]):
+                count[2] = 1
+            else:
+                count[2] = 0
+            if (lmList[16][2] < lmList[15][2] & lmList[15][2] < lmList[14][2] & lmList[14][2] < lmList[13][2]):
+                count[3] = 1
+            else:
+                count[3] = 0
+            if (lmList[20][2] < lmList[19][2] & lmList[19][2] < lmList[18][2] & lmList[18][2] < lmList[17][2]):
+                count[4] = 1
+            else:
+                count[4] = 0
+
+        print(sum(count))
+        cv2.putText(img,f"Count : {sum(count)}",(10,150),cv2.FONT_HERSHEY_PLAIN,3,(255,0,255),3)
 
         cv2.imshow("Image", finalImg)
 
